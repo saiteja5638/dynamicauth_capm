ca7777c2f9778b8d1fd9d3e78f640205fbd12e34
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-11-11 11:14:52 +0000

    new xsuaa commit

--- srv/server.js
+++ srv/server.js
@@ -49,6 +49,43 @@ cds.on("bootstrap", (app) => {
 });
 
 
+async function authenticate(req, next) {
+
+
+    if ((req.path.includes('user_information'))) {
+        const xsuaaService = xsenv.getServices({
+            uaa: {
+                name: 'capservsoauth' // Replace with the exact name of the desired service instance
+            }
+        });
+        const clientId = xsuaaService.uaa.clientid;
+        const clientSecret = xsuaaService.uaa.clientsecret;
+        const tokenUrl = xsuaaService.uaa.url + '/oauth/token';
+
+        const params = new URLSearchParams();
+        params.append('grant_type', 'client_credentials');
+        params.append('client_id', clientId);
+        params.append('client_secret', clientSecret);
+
+        await axios.post(tokenUrl, params)
+            .then(response => {
+                const accessToken = response.data.access_token;
+
+                req.headers.authorization = "Bearer " +
+                    accessToken;
+                next();
+            }).catch(error => {
+                console.log("Error obtaining access token:", error);
+                next();
+            });
+
+    }
+    else {
+        next();
+    }
+
+
+}
 
 // Start CDS server
 module.exports = cds.server;
